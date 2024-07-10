554c405844abbbc7ceec708b77b807a5257a2414
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-07-10 14:11:55 +0530

    First commit

--- src/app/component/Section2.jsx
+++ src/app/component/Section2.jsx
@@ -9,13 +9,13 @@ import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Section2() {
-    const textRef = useRef(null);
     const fontRef = useRef(null);
+    const textRef = useRef(null);
     
 
     useEffect(() => {
         gsap.fromTo(
-          textRef.current,
+          fontRef.current,
           { opacity: 0, y: 50 },
           {
             opacity: 1,
@@ -33,7 +33,7 @@ export default function Section2() {
       }, []);
 
       useEffect(() => {
-        const text = fontRef.current;
+        const text = textRef.current;
         const letters = text.innerText.split('');
         text.innerHTML = letters.map(letter => `<span>${letter}</span>`).join('');
     
@@ -61,14 +61,14 @@ export default function Section2() {
         <div className='section3'>
             <div className='section3-mx-w'>
                 <div className='text-left'>
-                    <h2 ref={fontRef} >EPROPELLED DESIGNS <img className="text-img" src='./home image/ph1.webp' /> STATE OF THE ART  MOTORS, <img className='text-img1' src='./home image/ph3.webp' /> GENERATORS, AND POWERMANAGEMENT SYSTEMS.</h2>
+                    <h2 ref={textRef} >EPROPELLED DESIGNS <img className="text-img" src='./home image/ph1.webp' /> STATE OF THE ART  MOTORS, <img className='text-img1' src='./home image/ph3.webp' /> GENERATORS, AND POWERMANAGEMENT SYSTEMS.</h2>
                 </div>
                 <div className='w100'>
                     <div className='flex justify-center w40'>
                         <img className='circle-1' src='./home image/circle.svg' />
                     </div>
                     <div className='w60'>
-                        <p ref={textRef} >Our technology helps reduce energy consumption and improve system efficiency at a lower cost in the aerospace, manned
+                        <p ref={fontRef} >Our technology helps reduce energy consumption and improve system efficiency at a lower cost in the aerospace, manned
                             and unmanned aerial vehicles, electric vehicles, and pump motor markets. We are a leader in magnetics engineering, and
                             our patented technology innovations are used in the air, land, and water, defining the future of electric propulsion.</p>
                     </div>
